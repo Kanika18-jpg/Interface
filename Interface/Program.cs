9ac58bfb977560c802336587dea753e3cb22c638
hp <sabhrwalkanika1999@gmail.com>
2023-10-07 15:58:29 +0530

    Add project files.

---
+++ Interface/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    interface i1
+    {
+        void show();
+    }
+    interface i2
+    {
+        void show2();
+    }
+    class A : i1, i2
+    {
+        public void show() {
+            Console.WriteLine("i1");
+        }
+        public void show2()
+        {
+            Console.WriteLine("i2");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            i1 obj1;
+            i2 obj2;
+            obj1 = new A();
+            obj2 = new A();
+            obj1.show();
+            obj2.show2();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Interface/interface2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    public interface i
+    {
+        void greet();
+    }
+    class B : i
+    {
+        public void greet() {
+            Console.WriteLine("Good Morning!");
+        }
+    }
+    class C : i
+    {
+        public void greet()
+        {
+            Console.WriteLine("Good Afternoon!");
+        }
+    }
+    class D : i
+    {
+        public void greet()
+        {
+            Console.WriteLine("Good Evening!");
+        }
+    }
+    internal class interface2
+    {
+        public static void main(string[] args)
+        {
+            i obj;
+            obj = new B();
+            obj.greet();
+            obj = new C();
+            obj.greet();
+            obj = new D();
+            obj.greet();
+            Console.ReadLine();
+        }
+    }
+}
